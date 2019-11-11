23bc1b56d633db1416959b4a027c099719922b08
soo0limm <stellar1198@gmail.com>
2019-11-11 23:02:53 +0900

    load my location - main

---
+++ Seed/app/src/main/java/com/example/seed/SearchMyLocation.java
@@ -0,0 +1,22 @@
+package com.example.seed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapView;
+
+public class SearchMyLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_my_location);
+
+//        MapView mapView = new MapView(this);
+//
+//        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.my_location);
+//        mapViewContainer.addView(mapView);
+    }
+}
