3083fd2562d373f1b97aa49b01bf0d69c4400b75
may kim <49mgntic@gmail.com>
2019-11-16 13:46:26 +0900

    write activity_buy_completed.xml

---
+++ Seed/app/src/main/java/com/example/seed/BuyCompletedActivity.java
@@ -0,0 +1,14 @@
+package com.example.seed;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BuyCompletedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_completed);
+    }
+}
