5279487ca814be42ffb0ed3c32918d69514defcb
may kim <49mgntic@gmail.com>
2019-11-16 10:49:37 +0900

    add basket_view BasketActivity, Adapter, Data, XML

---
+++ Seed/app/src/main/java/com/example/seed/OnBasketClickListener.java
@@ -0,0 +1,9 @@
+package com.example.seed;
+
+import android.view.View;
+
+// Customized by MS
+
+public interface OnBasketClickListener {
+    public void onItemClick(BasketAdapter.BasketViewHolder holder, View view, int position);
+}
---
+++ Seed/app/src/main/java/com/example/seed/data/BasketData.java
@@ -0,0 +1,63 @@
+package com.example.seed.data;
+
+// Customized by MS
+
+public class BasketData {
+
+    private int image;     // 추후 통신 시 자료형 수정할 것
+    private String name;
+    private int quantity;
+//    private Date timePickup;
+    private int salePrice;
+
+    private boolean vinyl;
+    private boolean paper;
+    private boolean box;
+
+    public BasketData(int image, String name, int quantity, int salePrice, boolean vinyl, boolean paper, boolean box) {
+        this.image = image;
+        this.name = name;
+        this.quantity = quantity;
+//        this.timePickup = timePickup;
+        this.salePrice = salePrice;
+
+        this.vinyl = vinyl;
+        this.paper = paper;
+        this.box = box;
+    }
+
+
+    public int getImage() { return image; }
+
+    public String getName() { return name; }
+
+    public int getQuantity() { return quantity; }
+
+//    public Date getTimePickup() { return timePickup; }
+
+    public int getSalePrice() { return salePrice; }
+
+    public boolean isVinyl() { return vinyl; }
+
+    public boolean isPaper() { return paper; }
+
+    public boolean isBox() { return box; }
+
+    public void setImage(int image) { this.image = image; }
+
+    public void setName(String name) { this.name = name; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+//    public void setTimePickup(Date timePickup) { this.timePickup = timePickup; }
+
+    public void setSalePrice(int salePrice) { this.salePrice = salePrice; }
+
+    public void setVinyl(boolean vinyl) { this.vinyl = vinyl; }
+
+    public void setPaper(boolean paper) { this.paper = paper; }
+
+    public void setBox(boolean box) { this.box = box; }
+
+}
+
